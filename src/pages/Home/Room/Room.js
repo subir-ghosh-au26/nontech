2ede458e083421a2ad19e3546e22e16c0ddb0c12
subir-ghosh-au26 <85132702+subir-ghosh-au26@users.noreply.github.com>
2025-01-21 12:09:47 +0530

    Update Room.js

--- src/pages/Home/Room/Room.js
+++ src/pages/Home/Room/Room.js
@@ -4,10 +4,10 @@ import { useParams } from 'react-router-dom'
 const Room = () => {
     const {roomId} = useParams();
     const meeting = async(element) =>{
-        const appID = 2024379751;
+        const appID = 946219318;
         // const AppId = 2024379751;
         // const AppSign = "f01b47da1b2abd43a821d3b1c09375b57356fc793668b20eb8e3b9aec532e673";
-        const serverSecret = "b1054c0dabcb1f097c5664659c5a6505";
+        const serverSecret = "8e0b853d79deae0bcbfe949b73ca46a4";
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
             appID,
             serverSecret,
